513a1bf3e6971d28ee236244c6c5a0fc2c4ea20f
Archer-sssyc <648766631@qq.com>
2021-08-26 13:20:47 +0000

    38->35

--- src/offline/movie/model-update-deepfm/deepfm_model.sh
+++ src/offline/movie/model-update-deepfm/deepfm_model.sh
@@ -4,7 +4,7 @@ echo " == begin python deepfm.py  =="
 cd /code/
 
 mkdir serve
-python deepfm.py --data_dir ./ --servable_model_dir serve --log_steps 10 --num_epochs 1 --field_size 38 --feature_size 117581 --deep_layers '2,2,2'
+python deepfm.py --data_dir ./ --servable_model_dir serve --log_steps 10 --num_epochs 1 --field_size 35 --feature_size 117581 --deep_layers '2,2,2'
 tar -cvf deepfm_model.tar  serve/
 gzip deepfm_model.tar
 echo " == done python deepfm.py  =="
